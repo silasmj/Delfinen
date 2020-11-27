e625c21a8dd39490370821c8761b74bca15686a0
silasmj <74252789+silasmj@users.noreply.github.com>
2020-11-27 11:22:26 +0100

    Add files via upload

---
+++ Delfinen.java
@@ -0,0 +1,8 @@
+public class Delfinen   {
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      ArrayList<Member> member = new ArrayList<>();
+      ArrayList<Trainer> trainer = new ArrayList<>();
+      ArrayList<Tournament> tournament = new ArrayList<>();
+   }
+}
