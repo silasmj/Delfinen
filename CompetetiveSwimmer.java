226350cd20c8aed6a01889519c4f598fabd0cae8
silasmj <74252789+silasmj@users.noreply.github.com>
2020-12-04 10:33:51 +0100

    Add files via upload

--- CompetetiveSwimmer.java
+++ CompetetiveSwimmer.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 
 public class CompetetiveSwimmer extends Member {
     private String swimStyle;
-    private double records;
     private ArrayList<Integer> listOfTournaments = new ArrayList<>();
     private int trainerId;
 
-    public CompetetiveSwimmer(String firstName, String lastName, int age, String email, int phoneNumber, int id, Boolean active, String swimStyle, double records){
+    public CompetetiveSwimmer(String firstName, String lastName, int age, String email, int phoneNumber, int id, Boolean active, String swimStyle){
       super(firstName, lastName, age, email, phoneNumber, id, active);
-      this.swimStyle = swimStyle;
-      this.records = records;
-        
+      this.swimStyle = swimStyle;   
+    }
+    
+    public CompetetiveSwimmer() {
     }
     
     public String toString() {
-      return "Competetive Swimmer" + "\nFirstname: " + getFirstName() + "\nLastname: " + getLastName() + "\nEmail: " + getEmail() + "\nPhone number: " + getPhoneNumber() + "\nId: " + getId() + "\nActive or passive: " + getActive() + "\nSwim style: " + swimStyle + "\nRecords: " + records;
+      return "Competetive Swimmer" + "\nFirstname: " + getFirstName() + "\nLastname: " + getLastName() + "\nEmail: " + getEmail() + "\nPhone number: " + getPhoneNumber() + "\nId: " + getId() + "\nActive or passive: " + getActive() + "\nSwim style: " + swimStyle;
     }
     
     public String getSwimStyle(){
@@ -24,14 +24,6 @@ public class CompetetiveSwimmer extends Member {
     public void setSwimStyle(String swimStyle){
       this.swimStyle = swimStyle;
     }
-    
-    public double getRecords(){
-      return records;
-    }
-    
-    public void setRecords(double records){
-      this.records = records;
-    }
-
+   
 
 }
